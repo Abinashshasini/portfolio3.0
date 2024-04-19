9bd94ed13ae36b37db6f345f12581a017a1fc33c
Abinash Shaini <abinashshasini125@gmail.com>
2024-04-19 15:55:31 +0530

    Skills secton work starts

---
+++ src/components/contact/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './style.module.scss';
+
+const Contact = () => {
+  return (
+    <div>
+      <p>Contact</p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { LogoIcn } from '@/utils';
 import classes from './style.module.scss';
-import { useGSAP } from '@gsap/react';
 
 const Header = () => {
+  /** Required states and refs */
+  const [showMenuButton, setShowMenuButton] = useState(false);
+
   /** GSAP hook for animation of list elements */
   useGSAP(() => {
     gsap.fromTo(
@@ -15,28 +18,59 @@ const Header = () => {
     );
   }, []);
 
+  /** Function to keep track of page scroll to show the hamburger menu */
+  const handleScroll = () => {
+    if (window && window.scrollY > 200) {
+      setShowMenuButton(true);
+    } else {
+      setShowMenuButton(false);
+    }
+  };
+
+  /** Effect to add scroll event listener */
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <header className={classes.container}>
-      <a aria-label="Bake to Home">
-        <Image src={LogoIcn} alt="logo" width={100} height={100} />
-      </a>
-      <ul className={classes.menuContainer}>
-        <a href="/#services">
-          <li id="hero_list-item">Services</li>
-        </a>
-        <a href="#work">
-          <li id="hero_list-item">Works</li>
-        </a>
-        <a href="/#about">
-          <li id="hero_list-item">About</li>
-        </a>
-        <a href="/#roles">
-          <li id="hero_list-item">Roles</li>
-        </a>
-        <a href="/#contact">
-          <li id="hero_list-item">Contact</li>
-        </a>
-      </ul>
+      {!showMenuButton && (
+        <>
+          <a aria-label="Bake to Home">
+            <Image src={LogoIcn} alt="logo" width={100} height={100} />
+          </a>
+          <ul className={classes.menuContainer}>
+            <a href="/#services">
+              <li id="hero_list-item">Services</li>
+            </a>
+            <a href="#work">
+              <li id="hero_list-item">Works</li>
+            </a>
+            <a href="/#about">
+              <li id="hero_list-item">About</li>
+            </a>
+            <a href="/#roles">
+              <li id="hero_list-item">Roles</li>
+            </a>
+            <a href="/#contact">
+              <li id="hero_list-item">Contact</li>
+            </a>
+          </ul>
+        </>
+      )}
+
+      <button
+        aria-label="Hamburger Menu button"
+        aria-expanded="false"
+        aria-controls="menu"
+        data-visible={showMenuButton}
+        className={classes.hamBurgerMenu}
+      >
+        <span></span>
+        <span></span>
+      </button>
     </header>
   );
 };
