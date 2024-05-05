15cf57d18c22a544dccff8dbf81744ac7b2a97ad
Abinash Shaini <abinashshasini125@gmail.com>
2024-05-05 11:18:26 +0530

    Skills and experience section work startd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,6 @@ export default function Home() {
       <MoreAboutMe />
       <Experience />
       <Reviews />
-      <div style={{ height: '100vh', background: 'yellow' }}></div>
     </main>
   );
 }
